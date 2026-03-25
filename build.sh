49b61cb7b0c29fcefca85e99378c377dec7bb270
VinhTin-AQUA <tinhovinh@gmail.com>
2026-03-25 09:14:42 +0700

    update workflow

--- build.sh
+++ build.sh
@@ -1,11 +1,7 @@
 # npm run tauri build -- --target x86_64-unknown-linux-gnu --no-bundle
 npm run tauri build --no-bundle
 
-TARGET_DIR="app_portable"
-TARGET_DIR_TAR="garage_dashboard_portable"
-
-rm -rf "$TARGET_DIR" "$TARGET_DIR_TAR"
-
+TARGET_DIR="garage_dashboard_portable"
 mkdir "$TARGET_DIR"
 
 cp -r src-tauri/target/release/app "$TARGET_DIR/"
@@ -13,4 +9,4 @@ cp -r src-tauri/target/release/resources "$TARGET_DIR/"
 cp -r src-tauri/target/release/.cargo-lock "$TARGET_DIR/"
 cp -r src-tauri/target/release/app.d "$TARGET_DIR/"
 
-echo "Done! Folder: $TARGET_DIR_TAR"
+echo "Done! Folder: $TARGET_DIR"
